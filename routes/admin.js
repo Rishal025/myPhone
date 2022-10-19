3afb98890f3dbb2fc7586e473fa5f5b0dae08c00
Rishal025 <103408996+Rishal025@users.noreply.github.com>
2022-10-19 23:42:51 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -36,7 +36,7 @@ router.get('/product', controllers.prodData)
 
 // // ============================GET ADD PRODUCTS===========================
 
-router.get('/add-product', controllers.addPr oducts)
+router.get('/add-product', controllers.addProducts)
 
 // // ==============================GET ADD PRODUCTS===============================
 
